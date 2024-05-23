d88ad851a13418175dee6ab393e35f6fd6d5f78f
sahilkodkar <sahilakodkar@gmail.com>
2024-05-24 00:03:48 +0530

    Model Evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from NLPTextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrai
 from NLPTextSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from NLPTextSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from NLPTextSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from NLPTextSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from NLPTextSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -46,6 +47,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_trainer = ModelTrainerTrainingPipeline()
    model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
